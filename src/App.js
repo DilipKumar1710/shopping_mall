cfee702c87cf8ea587fdae5678224ad0f27cddd1
DilipKumar1710 <dilipkumar.vaddepally141@gmail.com>
2023-05-25 14:04:39 +0530

    New Data is Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import RegisterForm from './Components/RegisterForm/index';
+import LoginForm from './Components/LoginForm/index';
+import Header from './Components/Header/index';
+import NotFound from './Components/NotFound/index';
+import ProtectedRoute from './Components/ProtectedRoute/index';
 import ShoppingMall from './Components/ShoppingMall/index';
 
 class App extends Component{
   render(){
     return (
-      <ShoppingMall/>
+      <>
+        <Header/>
+        <Switch>
+          <Route exact path = "/register" component = {RegisterForm}/>
+          <Route exact path = "/login" component = {LoginForm}/>
+          <ProtectedRoute exact path = "/" component = {ShoppingMall}/>
+          <Route component = {NotFound}/>
+        </Switch>
+      </>
     )
   }
 }
